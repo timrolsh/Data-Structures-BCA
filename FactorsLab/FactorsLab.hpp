d15505c8f0e411d8e37f44a511e56c075be24bd8
Tim Rolshud <tim.rolsh@gmail.com>
2024-03-26 16:56:57 -0400

    FACTORS: remove namespace from header

--- FactorsLab/FactorsLab.hpp
+++ FactorsLab/FactorsLab.hpp
@@ -3,10 +3,9 @@
 #define FACTORSLAB_FACTORSLAB_H
 
 #include <vector>
-using std::vector;
 unsigned long modularPower(unsigned long base, unsigned long exponent, unsigned long modulus);
 bool isPrime(unsigned long n);
 long findPrime(unsigned long n);
-vector<unsigned long> factor(unsigned long n);
+std::vector<unsigned long> factor(unsigned long n);
 
 #endif
