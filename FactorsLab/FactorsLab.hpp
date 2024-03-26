4af7891ed4900cec4da67e8f82bad4cfad6f4450
Tim Rolshud <tim.rolsh@gmail.com>
2024-03-26 10:25:28 -0400

    FACTORS: Add headers and cmake build file

---
+++ FactorsLab/FactorsLab.hpp
@@ -0,0 +1,11 @@
+#ifndef FACTORSLAB_FACTORSLAB_H
+
+#define FACTORSLAB_FACTORSLAB_H
+
+#include <vector>
+using std::vector;
+unsigned long modularPower(unsigned long base, unsigned long exponent, unsigned long modulus);
+bool isPrime(unsigned long n);
+vector<unsigned long> factor(unsigned long n);
+
+#endif
