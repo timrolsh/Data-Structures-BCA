6d58ca2c014974cc98edd1a19db01c9abdcbae0e
Tim Rolshud <tim.rolsh@gmail.com>
2024-03-26 18:11:00 -0400

    FACTORS: remove unused methods from header

--- FactorsLab/FactorsLab.hpp
+++ FactorsLab/FactorsLab.hpp
@@ -3,9 +3,6 @@
 #define FACTORSLAB_FACTORSLAB_H
 
 #include <vector>
-unsigned long modularPower(unsigned long base, unsigned long exponent, unsigned long modulus);
-bool isPrime(unsigned long n);
-long findPrime(unsigned long n);
 std::vector<unsigned long> factor(unsigned long n);
 
 #endif
