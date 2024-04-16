4b2c4e62d92d57eec451cd917d70d8c4fc96f001
Tim Rolshud <tim.rolsh@gmail.com>
2024-04-16 11:58:54 -0400

    FACTORS: add helper function tests for edge cases

--- FactorsLab/FactorsLab.hpp
+++ FactorsLab/FactorsLab.hpp
@@ -4,5 +4,7 @@
 
 #include <vector>
 std::vector<unsigned long> factor(unsigned long n);
-
+unsigned long modularPower(unsigned long base, unsigned long exponent, unsigned long modulus);
+unsigned long modularMultiply(unsigned long a, unsigned long b, unsigned long m);
+bool isPrime(unsigned long n);
 #endif
